9c738b123c5b19101351db0bbaf4ca771c5e06fa
targax <brunotarga08@gmail.com>
2025-02-14 18:31:20 -0300

    Codigo de calcular media de dois numeros

---
+++ calcula_media.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))   
+
+media = (numero1 + numero2) / 2
+
+print("A média dos números é: ", media)
