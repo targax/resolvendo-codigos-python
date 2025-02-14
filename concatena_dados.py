d9527a2f02e5709c80d66271825fca3d3afc4ff0
targax <brunotarga08@gmail.com>
2025-02-14 00:58:02 -0300

    Criação do arquivo de concatenar dados

---
+++ concatena_dados.py
@@ -0,0 +1,9 @@
+# Solicita dois dados do usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatena os dados
+resultado = dado1 + " " + dado2
+
+# Exibe o resultado
+print("Resultado da concatenação:", resultado)
