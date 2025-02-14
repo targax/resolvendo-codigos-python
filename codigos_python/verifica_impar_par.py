0f6bc01dcd12fbf399316d7657ff13ede740fef2
targax <brunotarga08@gmail.com>
2025-02-14 18:29:53 -0300

    Codiga que verifica se numero é par ou impar

---
+++ codigos_python/verifica_impar_par.py
@@ -0,0 +1,7 @@
+numeroInteiro = int(input("Digite um número inteiro: "))
+
+if numeroInteiro % 2 == 0:  
+    print("O número", numeroInteiro, "é par")
+else:
+    print("O número", numeroInteiro, "é ímpar") 
+
